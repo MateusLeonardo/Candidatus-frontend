ed5ca920245a3f458edcfa8f04dbb1f713e106ba
Mateus Leonardo <mateus_leonardo1997@hotmail.com>
2025-11-11 21:44:02 -0300

    Implement CidadesPage with city data fetching and loading state
    - Integrated `useGetAllCities` hook to fetch city data for the CidadesPage.
    - Added loading state representation using `CityTableSkeleton` while data is being fetched.
    - Enhanced the layout with `CityDialog` for city management and `CityTable` for displaying city information.

--- src/app/(dashboard)/dashboard/cidades/page.tsx
+++ src/app/(dashboard)/dashboard/cidades/page.tsx
@@ -1,3 +1,24 @@
+"use client";
+
+import { CityDialog } from "@/components/dialogs/city-dialog";
+import { CityTableSkeleton } from "@/components/skeletons/city-table-skeleton";
+import { CityTable } from "@/components/tables/city-table";
+import { useGetAllCities } from "@/hooks/queries/city/useGetAllCities";
+
 export default function CidadesPage() {
-  return <h1>Listagem de todas as cidades</h1>;
+  const { data: cities, isLoading: isLoadingCities } = useGetAllCities();
+
+  if (isLoadingCities) return <CityTableSkeleton />;
+
+  return (
+    <div className="my-8 px-8">
+      <div className="flex justify-between items-center">
+        <h1 className="text-2xl font-bold mb-6 text-start">Cidades</h1>
+        <CityDialog />
+      </div>
+      <div className="overflow-x-auto">
+        <CityTable cities={cities?.cities ?? []} />
+      </div>
+    </div>
+  );
 }
