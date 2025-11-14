2eeac6c1ccd2467330e70b9fb3a0d238b20a6076
Mateus Leonardo <mateus_leonardo1997@hotmail.com>
2025-11-14 16:43:23 -0300

    Add ConfirmDialog component for delete confirmation
    - Introduced the `ConfirmDialog` component to streamline the deletion process for cities and states.
    - Replaced inline alert dialogs in `CityTable` and `StateTable` with the new `ConfirmDialog` for improved code reusability and maintainability.
    - Integrated delete mutation handling within the `ConfirmDialog` to provide user feedback during deletion operations.

---
+++ src/components/dialogs/confirm-dialog.tsx
@@ -0,0 +1,50 @@
+import { Button } from "../ui/button";
+import { Trash } from "lucide-react";
+import { AlertDialog, AlertDialogTrigger, AlertDialogContent, AlertDialogHeader, AlertDialogTitle, AlertDialogFooter, AlertDialogCancel, AlertDialogAction } from "../ui/alert-dialog";
+import { UseMutationResult } from "@tanstack/react-query";
+import { AxiosError } from "axios";
+import { IResponseError } from "@/types/auth";
+
+interface ConfirmDialogProps {
+    deleteMutation: UseMutationResult<any, AxiosError<IResponseError, any>, number, unknown>;
+    id: number;
+    title: string;
+}
+export function ConfirmDialog({ deleteMutation, id, title }: ConfirmDialogProps) {
+    return (
+        <>
+            <AlertDialog>
+                <AlertDialogTrigger asChild>
+                    <Button
+                        variant="outline"
+                        size="icon"
+                        className="cursor-pointer"
+                    >
+                        <Trash className="w-4 h-4" />
+                    </Button>
+                </AlertDialogTrigger>
+                <AlertDialogContent>
+                    <AlertDialogHeader>
+                        <AlertDialogTitle>
+                            Deseja realmente remover {title}?
+                        </AlertDialogTitle>
+                    </AlertDialogHeader>
+                    <AlertDialogFooter>
+                        <AlertDialogCancel className="cursor-pointer">
+                            Cancelar
+                        </AlertDialogCancel>
+                        <AlertDialogAction
+                            className="cursor-pointer"
+                            onClick={() => deleteMutation.mutate(id)}
+                            disabled={deleteMutation.isPending}
+                        >
+                            {deleteMutation.isPending
+                                ? "Removendo..."
+                                : "Remover"}
+                        </AlertDialogAction>
+                    </AlertDialogFooter>
+                </AlertDialogContent>
+            </AlertDialog>
+        </>
+    )
+}
