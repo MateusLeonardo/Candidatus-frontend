b61199b88c70b330f6a6fee6af2cbdd589aebaf1
Mateus Leonardo <mateus_leonardo1997@hotmail.com>
2025-12-08 21:09:40 -0300

    feat: implement platforms page with table and dialog

--- src/app/(dashboard)/dashboard/plataformas/page.tsx
+++ src/app/(dashboard)/dashboard/plataformas/page.tsx
@@ -1,7 +1,22 @@
+"use client";
+import { PlatformDialog } from "@/components/platform/platform-dialog";
+import { TableSkeleton } from "@/components/skeletons/table-skeleton";
+import { PlatformTable } from "@/components/tables/platform-table";
+import { getAllPlatforms } from "@/hooks/queries/platform/get-all-platforms";
+
 export default function PlataformasPage() {
+  const { data, isLoading } = getAllPlatforms();
+
+  if (isLoading) return <TableSkeleton />;
   return (
-    <div>
-      <h1>Plataformas</h1>
+    <div className="my-8 px-8">
+      <div className="flex justify-between items-center">
+        <h1 className="text-2xl font-bold mb-6 text-start">Plataformas</h1>
+        <PlatformDialog />
+      </div>
+      <div className="overflow-x-auto">
+        <PlatformTable platforms={data?.platforms ?? []} />
+      </div>
     </div>
   );
 }
