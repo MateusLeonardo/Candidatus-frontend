12c2f816148dbdf9d689e35881072947d0063d6b
Mateus Leonardo <mateus_leonardo1997@hotmail.com>
2026-01-06 19:45:31 -0300

    feat: adding user context provider to providers

--- src/providers/providers.tsx
+++ src/providers/providers.tsx
@@ -1,16 +1,19 @@
+import { UserContextProvider } from "@/context/user-context";
 import { QueryProvider } from "./query-provider";
 import { ThemeProvider } from "./theme-provider";
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <QueryProvider>
-      <ThemeProvider
-        attribute="class"
-        defaultTheme="system"
-        enableSystem
-        disableTransitionOnChange
-      >
-        {children}
-      </ThemeProvider>
-    </QueryProvider>
+    <UserContextProvider>
+      <QueryProvider>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </QueryProvider>
+    </UserContextProvider>
   );
 }
