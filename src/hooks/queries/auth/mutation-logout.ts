43c5e0c16be3287ba589327c9392ba53c8d9a950
Mateus Leonardo <mateus_leonardo1997@hotmail.com>
2026-01-06 19:47:04 -0300

    feat: removing user context after logout

--- src/hooks/queries/auth/mutation-logout.ts
+++ src/hooks/queries/auth/mutation-logout.ts
@@ -2,13 +2,16 @@
 import { useMutation } from "@tanstack/react-query";
 import { useRouter } from "next/navigation";
 import Cookies from "js-cookie";
+import { useUserContext } from "@/context/user-context";
 
 export function mutationLogout() {
   const router = useRouter();
+  const { setUserToLocalStorage } = useUserContext();
   return useMutation({
     mutationFn: async () => {
       Cookies.remove("access_token");
       Cookies.remove("refresh_token");
+      setUserToLocalStorage(null);
       router.push("/login");
     },
   });
