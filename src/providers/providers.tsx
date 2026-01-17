16f4c02cc358cec85cf831c2ed4bc12c4197393a
Mateus Leonardo <mateus_leonardo1997@hotmail.com>
2026-01-17 16:05:20 -0300

    refactor: update Providers component to integrate Redux and UserLoader for improved user state management

--- src/providers/providers.tsx
+++ src/providers/providers.tsx
@@ -1,19 +1,27 @@
-import { UserContextProvider } from "./user-context";
+"use client";
 import { QueryProvider } from "./query-provider";
 import { ThemeProvider } from "./theme-provider";
+import { UserLoader } from "./user-loader";
+import { store } from "@/store/configure-store";
+import { Provider } from "react-redux";
+
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <UserContextProvider>
-      <QueryProvider>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          {children}
-        </ThemeProvider>
-      </QueryProvider>
-    </UserContextProvider>
+    // <UserContextProvider>
+    <Provider store={store}>
+      <UserLoader>
+        <QueryProvider>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+        </QueryProvider>
+      </UserLoader>
+      </Provider>
+    // </UserContextProvider>
   );
 }
