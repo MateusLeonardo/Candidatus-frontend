5473ff6a7e0ea3ceaabb71ed2f824351dc8eb54d
Mateus Leonardo <mateus_leonardo1997@hotmail.com>
2026-01-17 16:06:05 -0300

    feat: add UserMenu component for user account management with dropdown options

---
+++ src/components/layout/user-menu.tsx
@@ -0,0 +1,55 @@
+"use client"
+
+import { useSelector } from "react-redux"
+import { RootState } from "@/store/configure-store"
+import { DropdownMenu, DropdownMenuContent, DropdownMenuGroup, DropdownMenuItem, DropdownMenuTrigger } from "../ui/dropdown-menu";
+import { Button } from "../ui/button";
+import { Avatar, AvatarImage } from "../ui/avatar";
+import { LogOut, MoreVertical, User } from "lucide-react";
+import { mutationLogout } from "@/features/auth/hooks/mutation-logout";
+
+export function UserMenu() {
+  const { email } = useSelector((state: RootState) => state.user);
+  const { mutate: logout } = mutationLogout();
+  
+  if (!email) return null;
+  
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button
+          variant="ghost"
+          className="flex items-center gap-2 px-3 cursor-pointer"
+        >
+          <Avatar className="h-8 w-8 ring-2 ring-white dark:ring-slate-700">
+            <AvatarImage src="https://github.com/mateusleonardo.png" />
+          </Avatar>
+          <span className="hidden sm:inline-block text-sm font-medium">
+            {email}
+          </span>
+          <MoreVertical className="h-4 w-4" />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent
+        className="w-56 text-slate-700 dark:text-slate-200 border shadow-lg"
+        align="end"
+      >
+        <DropdownMenuGroup>
+          <DropdownMenuItem className="cursor-pointer focus:bg-accent hover:bg-accent hover:text-accent-foreground">
+            <User className="mr-2 h-4 w-4" />
+            <span>Minha conta</span>
+          </DropdownMenuItem>
+        </DropdownMenuGroup>
+        <DropdownMenuGroup>
+          <DropdownMenuItem
+            onClick={() => logout()}
+            className="cursor-pointer focus:bg-accent hover:bg-accent hover:text-accent-foreground"
+          >
+            <LogOut className="mr-2 h-4 w-4" />
+            <span>Sair</span>
+          </DropdownMenuItem>
+        </DropdownMenuGroup>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
